4380c5e696f7f2b21014f1b6f544fe75897686fd
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-10 12:08:01 -0300

    Buffers e Middlewares

---
+++ streams/buffer.js
@@ -0,0 +1,10 @@
+// Representação de um espaço na memória do computador,
+// usado para transitar dados de uma maneira muito rápido.
+
+// Salvar e Ler na memória de uma maneira muito performática.
+// Guarda os dados de uma maneira binária.
+
+
+const buffer = Buffer.from("Hello")
+
+console.log(buffer.toJSON())
