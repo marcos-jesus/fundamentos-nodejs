45f4744401b1b069a38b4382657585261939ee4a
Marcos Jesus <marcosjesus.dev@gmail.com>
2023-02-08 00:18:47 -0300

    add Regex for routes

--- src/utils/build.route.path.js
+++ src/utils/build.route.path.js
@@ -1,5 +1,5 @@
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/
+  const routeParametersRegex = /:([a-zA-Z]+)/g
 
-  
+  console.log(Array.from(path.matchAll(routeParametersRegex)))
 }
